b45a00d885913b044c418a29be5f631f140f6db9
maheshthippala <mahesh.cubex@gmail.com>
2018-05-19 20:02:56 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/jsonparsingtest/Employees.java
@@ -0,0 +1,15 @@
+package cubex.mahesh.jsonparsingtest;
+
+import java.util.ArrayList;
+
+public class Employees {
+    private ArrayList<Employee> employees;
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+}
